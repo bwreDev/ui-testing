4b3a1ed797c94c52c61dd3bdd52e79e8e722ac51
Colton Bonner <cbonner26118@gmail.com>
2021-01-01 12:56:21 -0700

    Build for display

--- src/components/SplashPage/index.js
+++ src/components/SplashPage/index.js
@@ -2,15 +2,17 @@ import React from 'react';
 import Hero from '../Hero';
 import Feature from '../Feature';
 import SpecialMenu from '../SpecialMenu';
+import Footer from '../Footer';
 
 function SplashPage() {
-	return (
-		<>
-			<Hero />
-			<Feature />
-			<SpecialMenu />
-		</>
-	);
+  return (
+    <>
+      <Hero />
+      <Feature />
+      <SpecialMenu />
+      <Footer />
+    </>
+  );
 }
 
 export default SplashPage;
